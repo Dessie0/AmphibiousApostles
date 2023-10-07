e0586d1229940a489a169e9be88b6bd79ee4b1a4
Dessie <dessieyt@gmail.com>
2023-10-06 23:26:02 -0400

    First animations

--- Assets/Scripts/Player/Frog.cs
+++ Assets/Scripts/Player/Frog.cs
@@ -1,6 +1,7 @@
 using System;
 using Interactables;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -19,19 +20,35 @@ namespace Player
         private Vector2 facing;
         private Vector2 targetPosition;
     
-        private bool moving;
+        private Animator animator;
+        private SpriteRenderer spriteRenderer;
 
-        private PlayerInput input;
+        //Used in the editor for an animation event, tells the sprite when to actually move during the animation.
+        private bool animationToggle;
+
+        //Used to track if the sprite should be flipped.
+        //Cannot use facing as facing will flip the sprite when they move down.
+        private bool isLookingLeft;
         
+        private bool moving;
+        private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+        private static readonly int Direction = Animator.StringToHash("Direction");
+
         // Start is called before the first frame update
         void Start()
         {
             this.targetPosition = this.GetPosition();
-            this.input = this.GetComponent<PlayerInput>();
-
-            if (this.input == null)
+            this.animator = this.GetComponent<Animator>();
+            this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+            
+            if (this.animator == null)
             {
-                throw new Exception("Cannot find PlayerInput component.");
+                throw new Exception("Animator does not exist!");
+            }
+            
+            if (this.spriteRenderer == null)
+            {
+                throw new Exception("SpriteRenderer does not exist!");
             }
         }
 
@@ -49,8 +66,12 @@ namespace Player
                 this.facing = this.direction;
             }
             
-            //Check if there's a collider in front of the player.
-            this.moving = CheckCanMove();
+            //Set the animation bool if they're moving up so the appropriate animation plays.
+            this.animator.SetBool(IsMoving, this.moving);
+            this.animator.SetInteger(Direction, Math.Abs(this.facing.y - 1f) < 0.01 ? 1 : 0);
+            
+            //Flip the sprite
+            this.spriteRenderer.flipX = Math.Abs(this.direction.x - -1f) < 0.01 || !(Math.Abs(this.direction.x - 1) < 0.01) && this.spriteRenderer.flipX;
         }
         
         public void OnInteract(InputAction.CallbackContext context)
@@ -108,20 +129,43 @@ namespace Player
         // Update is called once per frame
         private void FixedUpdate()
         {
-            Vector3 position = this.transform.position;
+            if (!this.animationToggle) return;
             
+            Vector3 position = this.transform.position;
+            //Check if the player has reached their target position of the next tile
             if (Vector2.Distance(this.GetPosition(), this.targetPosition) < this.gridThreshold)
             {
+                //Lock the position to the tile
                 this.transform.position = new Vector3(this.targetPosition.x, this.targetPosition.y, position.z);
                 
-                if(!this.moving || !this.CheckCanMove()) return;
-                this.targetPosition = this.GetPosition() + new Vector2(this.direction.x, this.direction.y);
+                //Check if they're still moving, or if they even CAN move in the first place
+                if(!this.moving || !this.CheckCanMove())
+                {
+                    this.moving = false;
+                    this.animator.SetBool(IsMoving, false);
+                }
+                else
+                {
+
+                    this.animationToggle = false;
+                    
+                    //Otherwise, set the next target position.
+                    this.targetPosition = this.GetPosition() + new Vector2(this.direction.x, this.direction.y);
+                }
             }
             else
             {
+                //Slowly animate the position to the new location.
                 this.transform.position = Vector3.Lerp(position, new Vector3(this.targetPosition.x, this.targetPosition.y, position.z), this.speed);
             }
         }
+
+
+        private void AnimationShouldMove()
+        {
+            this.animationToggle = true;
+        }
+        
         
     }
 }
